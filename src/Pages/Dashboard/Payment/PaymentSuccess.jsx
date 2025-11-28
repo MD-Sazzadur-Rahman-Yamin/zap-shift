22726f8549359ec06d887af8d54d0ca7bb5b3442
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-11-28 18:52:47 +0600

    varify conform

--- src/Pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/Pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { useEffect } from 'react';
+import { Link, useSearchParams } from 'react-router';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 
 const PaymentSuccess = () => {
+  const axiosSecure = useAxiosSecure()
+  const [searchParams] = useSearchParams();
+  const sessionId = searchParams.get("session_id");
+  useEffect(() => {
+    if (sessionId) {
+      axiosSecure.patch(`/payment-success?session_id=${sessionId}`).then(res=> console.log(res.data));
+    }
+  }, [sessionId, axiosSecure]);
     return (
       <div className="page-body flex justify-center items-center flex-col gap-4">
         <h2 className="page-title">Payment Successful</h2>
