6b72123fe61303cf7dc13ed7ea8eb6654ca37a3a
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-12-04 00:20:29 +0600

    load riders data and assign a rider for parcels

--- src/Pages/Dashboard/AssignRiders/AssignRiders.jsx
+++ src/Pages/Dashboard/AssignRiders/AssignRiders.jsx
@@ -1,12 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { toast } from "react-toastify";
 
 const AssignRiders = () => {
   const axiosSecure = useAxiosSecure();
   const riderModalRef = useRef();
+  const [selectedParcel, setSelectedParcel] = useState(null);
 
-  const { data: parcels = [] } = useQuery({
+  const { data: parcels = [], refetch: parcelsRegetch } = useQuery({
     queryKey: ["parcels", "pending-pickup"],
     queryFn: async () => {
       const res = await axiosSecure.get(
@@ -15,9 +17,38 @@ const AssignRiders = () => {
       return res.data;
     },
   });
+  const { data: riders = [] } = useQuery({
+    queryKey: ["riders", selectedParcel?.senderDistrict, "available"],
+    enabled: !!selectedParcel,
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/riders?status=approved&workStatus=available&district=${selectedParcel.senderDistrict}`
+      );
+      return res.data;
+    },
+  });
 
-  const openAssignRiderModal = (parent) => {
+  const openAssignRiderModal = (parcel) => {
+    setSelectedParcel(parcel);
     riderModalRef.current.showModal();
+  };
+
+  const handleAssignRider = (rider) => {
+    const riderAssignInfo = {
+      riderId: rider._id,
+      riderEmail: rider.email,
+      riderName: rider.name,
+      parcelId: selectedParcel._id,
+    };
+    axiosSecure
+      .patch(`/parcels/${selectedParcel._id}`, riderAssignInfo)
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          riderModalRef.current.close();
+          parcelsRegetch();
+          toast("Rider has been assigned");
+        }
+      });
   };
 
   const tableTitles = (
@@ -51,7 +82,7 @@ const AssignRiders = () => {
                     onClick={() => openAssignRiderModal(parcel)}
                     className="btn btn-outline hover:text-base-100"
                   >
-                    Assign Riders
+                    Find Riders
                   </button>
                 </td>
               </tr>
@@ -65,14 +96,44 @@ const AssignRiders = () => {
         className="modal modal-bottom sm:modal-middle"
       >
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">
-            Press ESC key or click the button below to close
-          </p>
+          <h3 className="font-bold text-lg">Rider found:{riders.length}</h3>
+          <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Job</th>
+                  <th>Favorite Color</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row */}
+                {riders.map((rider, i) => (
+                  <tr key={rider._id}>
+                    <th>{i + 1}</th>
+                    <td>{rider.name}</td>
+                    <td>{rider.email}</td>
+                    <td>
+                      <button
+                        onClick={() => handleAssignRider(rider)}
+                        className="btn btn-outline hover:text-base-100"
+                      >
+                        Assign
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
-              <button className="btn btn-outline hover:text-base-100">Close</button>
+              <button className="btn btn-outline hover:text-base-100">
+                Close
+              </button>
             </form>
           </div>
         </div>
