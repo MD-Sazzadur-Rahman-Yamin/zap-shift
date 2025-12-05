e245c9ec9d8aee14088c8316dc71f22751dd87a5
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-12-05 17:54:40 +0600

    show rider compleated deliveries

--- src/Pages/Dashboard/CompletedDeliveries/CompletedDeliveries.jsx
+++ src/Pages/Dashboard/CompletedDeliveries/CompletedDeliveries.jsx
@@ -1,9 +1,68 @@
 import React from "react";
+import useAuth from "../../../Hooks/useAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const CompletedDeliveries = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: parcels = [] } = useQuery({
+    queryKey: ["parcles", user.email, "driver_assigned"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/parcels/rider?riderEmail=${user.email}&deliveryStatus=parcel_delivered`
+      );
+      return res.data;
+    },
+  });
+
+  console.log(parcels)
+
+  const calculatePayout = (parcel) => {
+    if (parcel.senderDistrict === parcel.receiverDistrict) {
+      return parcel.cost * 0.8;
+    } else {
+      return parcel.cost * 0.6;
+    }
+  };
+
   return (
     <div className="page-body">
-      <h2 className="page-title">Completed Deliveries</h2>
+      <h2 className="page-title">Completed Deliveries: {parcels.length}</h2>
+      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Created At</th>
+              <th>Pickup Diatrict</th>
+              <th>Cost</th>
+              <th>Payout</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row */}
+            {parcels.map((parcel, i) => (
+              <tr key={parcel._id}>
+                <th>{i + 1}</th>
+                <td>{parcel.parcelName}</td>
+                <td>{parcel.createAt}</td>
+                <td>{parcel.senderDistrict}</td>
+                <td>{parcel.cost}</td>
+                <td>{calculatePayout(parcel)}</td>
+                <td>
+                  <button className="btn btn-outline hover:text-base-100">
+                    Cash Out
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
